2644ecfd1fdd9d0c12a103f311429c1c64f23018
kyle cieskiewicz <kylecieskiewicz@kyles-MacBook-Pro.local>
2017-10-04 07:50:33 -0500

    completed assessment

--- mainCtrl.js
+++ mainCtrl.js
@@ -1,9 +1,78 @@
 module.exports = {
-
-
-
-
-
-
-
-}
+    
+        addUser(req, res) {
+          const db = req.app.get("db");
+          db.addUser([req.body.name, req.body.email]).then(user => {
+            res.json(user);
+          });
+        },
+        addVehicle(req, res) {
+          const db = req.app.get("db");
+          db.addVehicle([req.body.make,req.body.model,req.body.year, req.body.owner_id]).then(vehicle => {res.json(vehicle);});
+        },
+        countVehiclesByOwner(req, res) {
+          const db = req.app.get("db");
+          db.countVehiclesByOwner(req.params.userId).then(vehicles => {
+            res.json(vehicles);
+          });
+        },
+        getVehiclesByID(req, res) {
+          const db = req.app.get("db");
+          db.getVehiclesByID(req.params.userId).then(vehicles => {
+            res.json(vehicles);
+          });
+        },
+        getVehiclesByQuery(req, res) {
+          const db = req.app.get("db");
+          if (req.query.userEmail) {
+            return db.getVehiclesByEmail(req.query.userEmail).then(vehicles => {
+              return res.json(vehicles);
+            });
+          }
+          if (req.query.userFirstStart) {
+            return db
+              .searchByName(req.query.userFirstStart + "%")
+              .then(name => {
+                return res.json(name);
+              });
+          }
+        },
+        getVehiclesByYear(req, res) {
+          const db = req.app.get("db");
+          db.getVehiclesByYear().then(vehicles => {
+            res.json(vehicles);
+          });
+        },
+        getAllUsers(req, res) {
+          const db = req.app.get("db");
+          db.getAllUsers().then(users => {
+            res.json(users);
+          });
+        },
+        getAllVehicles(req, res) {
+          const db = req.app.get("db");
+          db.getAllVehicles().then(vehicles => {
+            res.json(vehicles);
+          });
+        },
+        changeOwner(req, res) {
+          const db = req.app.get("db");
+          db
+            .changeOwner([req.params.vehicleId, req.params.userId])
+            .then(owner => {
+              res.json(owner);
+            });
+        },
+        deleteOwner(req, res) {
+          const db = req.app.get("db");
+          db.deleteOwner([req.params.vehicleId]).then(owner => {
+            res.json(owner);
+          });
+        },
+        deleteVehicle(req, res) {
+          const db = req.app.get("db");
+          db.deleteVehicle([req.params.vehicleId]).then(id => {
+            res.json(id);
+          });
+        }
+      };
--- server.js
+++ server.js
@@ -1,11 +1,7 @@
-********* commit to track time ************
-
-
-
 const express = require('express')
-    , bodyParser = require('body-parser')
-    , cors = require('cors')
-    , massive = require('massive');
+, bodyParser = require('body-parser')
+, cors = require('cors')
+, massive = require('massive');
 
 const mainCtrl = require('./mainCtrl');
 
@@ -17,26 +13,36 @@ app.use(cors());
 // You need to complete the information below to connect
 // to the assessbox database on your postgres server.
 massive({
-  host: //host,
-  port: //port,
-  database: //database,
-  user: //user,
-  password: //password
-}).then( db => {
-  app.set('db', db);
+host: "localhost",
+port: 5432, //port,
+database: "assessbox", //database,
+user: "kylecieskiewicz", //user,
+password: "" //password
+}).then(db => {
+app.set("db", db);
 
-  // Initialize user table and vehicle table.
-  db.init_tables.user_create_seed().then( response => {
-    console.log('User table init');
-    db.init_tables.vehicle_create_seed().then( response => {
-      console.log('Vehicle table init');
-    })
-  })
-
-})
+// Initialize user table and vehicle table.
+db.init_tables.user_create_seed().then(response => {
+console.log("User table init");
+db.init_tables.vehicle_create_seed().then(response => {
+  console.log("Vehicle table init");
+});
+});
+});
 
 
 // ===== Build enpoints below ============
+app.post("/api/users", mainCtrl.addUser);
+app.post("/api/vehicles", mainCtrl.addVehicle);
+app.get("/api/user/:userId/vehiclecount", mainCtrl.countVehiclesByOwner);
+app.get("/api/user/:userId/vehicle", mainCtrl.getVehiclesByID);
+app.get("/api/vehicle", mainCtrl.getVehiclesByQuery);
+app.get("/api/newervehiclesbyyear", mainCtrl.getVehiclesByYear);
+app.get("/api/users", mainCtrl.getAllUsers);
+app.get("/api/vehicles", mainCtrl.getAllVehicles);
+app.put("/api/vehicle/:vehicleId/user/:userId", mainCtrl.changeOwner);
+app.delete("/api/user/:userId/vehicle/:vehicleId", mainCtrl.deleteOwner);
+app.delete("/api/vehicle/:vehicleId", mainCtrl.deleteVehicle);
 
 
 
@@ -47,5 +53,5 @@ massive({
 // ===== Do not change port ===============
 const port = 3000;
 app.listen(port, () => {
-  console.log('Listening on port: ', port);
+console.log('Listening on port: ', port);
 })
